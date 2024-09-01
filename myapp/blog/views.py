d0f9e19401b906be9755135522ca21eb48a16a91
mdriz22 <mohamedriyaz5050@gmail.com>
2024-09-01 21:20:35 +0530

    1/9/24

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello World, Your are at blog's index")
+
+
+def detail(request, post_id):
+    return HttpResponse(f"You are viewing post detail page and post id is {post_id}")
+
+def old_url_redirect(request):
+    return redirect("new_url")
+
+def new_url_view(request):
+    return HttpResponse("This is the new url")
